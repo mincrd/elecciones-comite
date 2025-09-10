b8ec30e7cfb2baacb29c769b451b2e2008c3453d
Kaking Choi <kakingchoi02@gmail.com>
2025-09-10 16:21:42 -0400

    Remove entrypoint.sh for Azure compatibility

--- api-votacion/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Reemplaza la configuración por defecto de Apache con la nuestra
-cp /home/site/wwwroot/laravel.conf /etc/apache2/sites-enabled/000-default.conf
-
-# Inicia el servidor Apache
-apache2-foreground
